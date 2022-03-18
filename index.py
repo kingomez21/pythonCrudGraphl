1e6254cdcfa93d0bc5fb2e648139ee65f85a3ef2
kingomez21 <juansebastiangomezmezu@gmail.com>
2022-03-18 11:12:42 -0500

    eliminacion de archivo innecesario llamado schema.py

--- index.py
+++ index.py
@@ -2,7 +2,8 @@ import graphene
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from starlette_graphene3 import GraphQLApp, make_playground_handler
-from schema import Query, Mutation, Subscription
+from controllers.UsuarioGqlController import UsuarioQuery, UsuarioMutation
+from controllers.LoginGqlController import LoginQuery, LoginMutation
 
 app = FastAPI()
 
@@ -12,6 +13,15 @@ app.add_middleware(CORSMiddleware,
     allow_headers=["*"],
 )
 
-schema = graphene.Schema(query=Query, mutation=Mutation, subscription=Subscription)
+class Mutation(UsuarioMutation, LoginMutation):
+    pass
+
+
+#QUERYS
+class Query(UsuarioQuery, LoginQuery):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
 app.add_route('/gql', GraphQLApp(schema=schema,context_value=Request ,on_get=make_playground_handler()))
